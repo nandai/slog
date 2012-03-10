c5ebbcee82e819a1734ca71efc9791c0455cfe63
SHINOHARA Yuki <coffeemam@gmail.com>
2012-03-10 13:08:21 +0900

    日時を変えずにICS環境へコピーするようスクリプトを修正

--- toics.sh
+++ toics.sh
@@ -2,16 +2,17 @@
 
 if [ $# -ne 1 ] ; then
     echo "usage: $0 ICS-top-dir"
+    echo
     exit 1
 fi
 
-mkdir -p                            $1/vendor/log-tools/slog/prebuilt
-cp bin/Android/slogsvc              $1/vendor/log-tools/slog/prebuilt/
-cp bin/Android/libslog.so           $1/vendor/log-tools/slog/prebuilt/
-cp src/SequenceLogService/slog.conf $1/vendor/log-tools/slog/
+mkdir -p                               $1/vendor/log-tools/slog/prebuilt
+cp -p bin/Android/slogsvc              $1/vendor/log-tools/slog/prebuilt/
+cp -p bin/Android/libslog.so           $1/vendor/log-tools/slog/prebuilt/
+cp -p src/SequenceLogService/slog.conf $1/vendor/log-tools/slog/
 
-mkdir -p                            $1/bionic/libc/include/slog
-cp include/slog/slog.h              $1/bionic/libc/include/slog/
-cp include/slog/SequenceLog.h       $1/bionic/libc/include/slog/
+mkdir -p                               $1/bionic/libc/include/slog
+cp -p include/slog/slog.h              $1/bionic/libc/include/slog/
+cp -p include/slog/SequenceLog.h       $1/bionic/libc/include/slog/
 
-cp -rf src/SequenceLogLib/net       $1/libcore/luni/src/main/java/
+cp -prf src/SequenceLogLib/net         $1/libcore/luni/src/main/java/
