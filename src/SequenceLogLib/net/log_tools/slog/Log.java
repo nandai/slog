6dd23429d9889d7b14807d31d8967f71841a1259
SHINOHARA Yuki <coffeemam@gmail.com>
2012-04-15 15:56:03 +0900

    TABをスペースに変換

--- src/SequenceLogLib/net/log_tools/slog/Log.java
+++ src/SequenceLogLib/net/log_tools/slog/Log.java
@@ -17,65 +17,65 @@ package net.log_tools.slog;
 
 public final class Log
 {
-	// 出力フラグ
-	public	static final int KEEP =			0;		// シーケンスログの出力をキープする
-	public	static final int OUTPUT_ALL =	1;		// キープ中のシーケンスログも含め出力する
-	public	static final int ALWAYS =		2;		// キープ中のシーケンスログを出力し、さらに配下のシーケンスログは即座に出力する
-	public	static final int ROOT =			3;		// シーケンスログサービスでルートをオンにするとALWAYSとして、オフにするとKEEPとして扱う
+    // 出力フラグ
+    public  static final int KEEP =         0;      // シーケンスログの出力をキープする
+    public  static final int OUTPUT_ALL =   1;      // キープ中のシーケンスログも含め出力する
+    public  static final int ALWAYS =       2;      // キープ中のシーケンスログを出力し、さらに配下のシーケンスログは即座に出力する
+    public  static final int ROOT =         3;      // シーケンスログサービスでルートをオンにするとALWAYSとして、オフにするとKEEPとして扱う
 
-	// ログレベル
-	private	static final int DEBUG =		0;		// デバッグ
-	private	static final int INFO =			1;		// 情報
-	private	static final int WARN =			2;		// 警告
-	private	static final int ERROR =		3;		// エラー
+    // ログレベル
+    private static final int DEBUG =        0;      // デバッグ
+    private static final int INFO =         1;      // 情報
+    private static final int WARN =         2;      // 警告
+    private static final int ERROR =        3;      // エラー
 
-	/**
-	 * シーケンスログファイル名設定
-	 */
-	public native static void setFileName(String name);
-	public native static void setRootFlag(int outputFlag);
+    /**
+     * シーケンスログファイル名設定
+     */
+    public native static void setFileName(String name);
+    public native static void setRootFlag(int outputFlag);
 
-	/**
-	 * ステップイン
-	 */
-	public         static long stepIn(String className, String funcName) {return stepIn(className, funcName, KEEP);}
-	public  native static long stepIn(String className, String funcName, int outputFlag);
+    /**
+     * ステップイン
+     */
+    public         static long stepIn(String className, String funcName) {return stepIn(className, funcName, KEEP);}
+    public  native static long stepIn(String className, String funcName, int outputFlag);
 
-	public         static long stepIn(int classID,      String funcName) {return stepIn(classID,   funcName, KEEP);}
-	public  native static long stepIn(int classID,      String funcName, int outputFlag);
+    public         static long stepIn(int classID,      String funcName) {return stepIn(classID,   funcName, KEEP);}
+    public  native static long stepIn(int classID,      String funcName, int outputFlag);
 
-	public         static long stepIn(int classID,      int    funcID)   {return stepIn(classID,   funcID,   KEEP);}
-	public  native static long stepIn(int classID,      int    funcID,   int outputFlag);
+    public         static long stepIn(int classID,      int    funcID)   {return stepIn(classID,   funcID,   KEEP);}
+    public  native static long stepIn(int classID,      int    funcID,   int outputFlag);
 
-	/**
-	 * ステップアウト
-	 */
-	public native static void stepOut(long slog);
+    /**
+     * ステップアウト
+     */
+    public native static void stepOut(long slog);
 
-	/**
-	 * メッセージ出力
-	 */
-	private native static void message(int level, String message,   long slog/*tag*/);
-	private native static void message(int level, int    messageID, long slog/*tag*/);
+    /**
+     * メッセージ出力
+     */
+    private native static void message(int level, String message,   long slog/*tag*/);
+    private native static void message(int level, int    messageID, long slog/*tag*/);
 
     private native static void message(int level, String message,   String tag);
 
-	public static void v(long slog/*tag*/, String message) {message(DEBUG, message,   slog);}
-	public static void d(long slog/*tag*/, String message) {message(DEBUG, message,   slog);}
-	public static void i(long slog/*tag*/, String message) {message(INFO,  message,   slog);}
-	public static void w(long slog/*tag*/, String message) {message(WARN,  message,   slog);}
-	public static void e(long slog/*tag*/, String message) {message(ERROR, message,   slog);}
+    public static void v(long slog/*tag*/, String message)              {message(DEBUG, message,   slog);}
+    public static void d(long slog/*tag*/, String message)              {message(DEBUG, message,   slog);}
+    public static void i(long slog/*tag*/, String message)              {message(INFO,  message,   slog);}
+    public static void w(long slog/*tag*/, String message)              {message(WARN,  message,   slog);}
+    public static void e(long slog/*tag*/, String message)              {message(ERROR, message,   slog);}
 
-	public static void v(long slog/*tag*/, int messageID)  {message(DEBUG, messageID, slog);}
-	public static void d(long slog/*tag*/, int messageID)  {message(DEBUG, messageID, slog);}
-	public static void i(long slog/*tag*/, int messageID)  {message(INFO,  messageID, slog);}
-	public static void w(long slog/*tag*/, int messageID)  {message(WARN,  messageID, slog);}
-	public static void e(long slog/*tag*/, int messageID)  {message(ERROR, messageID, slog);}
+    public static void v(long slog/*tag*/, int messageID)               {message(DEBUG, messageID, slog);}
+    public static void d(long slog/*tag*/, int messageID)               {message(DEBUG, messageID, slog);}
+    public static void i(long slog/*tag*/, int messageID)               {message(INFO,  messageID, slog);}
+    public static void w(long slog/*tag*/, int messageID)               {message(WARN,  messageID, slog);}
+    public static void e(long slog/*tag*/, int messageID)               {message(ERROR, messageID, slog);}
 
     // Androidに適用する時にandroid.util.Log()として修正しなくても良いようにするためのメソッド。
     // Throwableは無視する。
-	public static void w(long slog/*tag*/, String message, Throwable e) {message(WARN,  message, slog);}
-	public static void e(long slog/*tag*/, String message, Throwable e) {message(ERROR, message, slog);}
+    public static void w(long slog/*tag*/, String message, Throwable e) {message(WARN,  message, slog);}
+    public static void e(long slog/*tag*/, String message, Throwable e) {message(ERROR, message, slog);}
 
     public static void v(String tag,       String message)              {message(DEBUG, message,   tag);}
     public static void d(String tag,       String message)              {message(DEBUG, message,   tag);}
