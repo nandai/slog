faf3f3543593f8a515f7ac2bfb96100056b349c9
nandai <coffeemam@gmail.com>
2013-05-04 18:40:18 +0900

    phpで共有ライブラリが使用できない場合に使う、ダミーファイルを追加

---
+++ bin/PHP/slog.php
@@ -0,0 +1,11 @@
+<?php
+function slogSetFileName($name) {}
+function slogSetServiceAddress($address) {}
+function slogEnableOutput($enable) {}
+function slogStepIn($className, $funcName) {}
+function slogStepOut($slog) {}
+function slogD($slog, $message) {}
+function slogI($slog, $message) {}
+function slogW($slog, $message) {}
+function slogE($slog, $message) {}
+?>
